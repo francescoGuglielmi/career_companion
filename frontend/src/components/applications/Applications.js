8f47ae4d1faa72a55906aef9f21432861356f959
Sarah <sarahdavies186@gmail.com>
2023-04-26 22:32:52 +0100

    updated application  with date

--- frontend/src/components/applications/Applications.js
+++ frontend/src/components/applications/Applications.js
@@ -7,7 +7,7 @@ const Application = ({ application }) => {
       <h2>Applications</h2>
       <article data-cy="application" key={application._id}>
         {console.log(application)}
-        {application.company} - {application.jobTitle} - {application.location}
+        {application.company} - {application.jobTitle} - {application.location} - Applied on {application.createdAt}
       </article>
     </>
   );
