580730df2a5fa40521ee66ad32130e7977cd5588
Sarah <sarahdavies186@gmail.com>
2023-04-26 12:04:48 +0100

    updated sign up form frontend

--- frontend/src/components/user/SignUpForm.js
+++ frontend/src/components/user/SignUpForm.js
@@ -1,45 +1,114 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const SignUpForm = ({ navigate }) => {
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    fetch( '/users', {
-      method: 'post',
+    const formData = new FormData();
+    formData.append("email", email);
+    formData.append("password", password);
+    formData.append("firstName", firstName);
+    formData.append("lastName", lastName);
+
+    let response = await fetch("/users", {
+      method: "post",
       headers: {
-        'Content-Type': 'application/json',
+        // "Content-Type": "application/json",
       },
-      body: JSON.stringify({ email: email, password: password })
-    })
-      .then(response => {
-        if(response.status === 201) {
-          navigate('/login')
-        } else {
-          navigate('/signup')
-        }
-      })
-  }
+      body: formData,
+    });
+
+    if (response.status === 201) {
+      navigate("/login");
+    } else {
+      navigate("/signup");
+    }
+  };
 
   const handleEmailChange = (event) => {
-    setEmail(event.target.value)
-  }
+    setEmail(event.target.value);
+  };
 
   const handlePasswordChange = (event) => {
-    setPassword(event.target.value)
-  }
+    setPassword(event.target.value);
+  };
 
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
+  };
 
-    return (
-      <form onSubmit={handleSubmit}>
-          <input placeholder="Email" id="email" type='text' value={ email } onChange={handleEmailChange} />
-          <input placeholder="Password" id="password" type='password' value={ password } onChange={handlePasswordChange} />
-        <input id='submit' type="submit" value="Submit" />
-      </form>
-    );
-}
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value);
+  };
+
+  return (
+    <div className="flex flex-col items-start justify-start h-screen md:pl-60 md:pt-40">
+      <h1 className="text-5xl font-epilogue-regular text-black pt-4">
+        Sign up
+      </h1>
+      <div className="w-full max-w-xs">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-white px-2 pt-6 pb-8 mb-4 font-dm-sans-regular"
+        >
+          <div className="mb-4">
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="text"
+              placeholder="first name"
+              value={firstName}
+              onChange={handleFirstNameChange}
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="text"
+              placeholder="last name"
+              value={lastName}
+              onChange={handleLastNameChange}
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="text"
+              placeholder="email"
+              value={email}
+              onChange={handleEmailChange}
+            />
+          </div>
+          <div className="mb-6">
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-orange hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Sign Up
+            </button>
+          </div>
+          <p className="pt-4 text-sm">Already have an account? Sign in now</p>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default SignUpForm;
