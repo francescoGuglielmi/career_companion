e5fe014f7005ac96262eafc4f3075e3068563293
Sarah <sarahdavies186@gmail.com>
2023-05-01 09:57:56 +0100

    updated application status to string of options instead of boolean

--- frontend/src/components/applications/Applications.js
+++ frontend/src/components/applications/Applications.js
@@ -6,7 +6,7 @@ const Application = ({ application }) => {
       <article data-cy="application" key={application._id}>
         {console.log(application)}
         {application.company} - {application.jobTitle} - {application.location} - Applied on {application.createdAt} - <a
-              className="bg-transparent py-1 px-4 border border-black mt-2"
+              className="bg-transparent py-1 px-2 border border-black mt-2"
               href={application.link}
             >
               Link
