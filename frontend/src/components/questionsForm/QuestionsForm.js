dc15d1c988077922c768d7ae36651f9f978b4aee
Sarah <sarahdavies186@gmail.com>
2023-05-03 20:27:26 +0100

    styling the interview dojo page

--- frontend/src/components/questionsForm/QuestionsForm.js
+++ frontend/src/components/questionsForm/QuestionsForm.js
@@ -1,29 +1,54 @@
-import './QuestionsForm.css'
+import "./QuestionsForm.css";
 
 function QuestionsForm(props) {
-
   return (
     <>
       <form onSubmit={props.handleAnswersSubmit}>
         <h3>Question {props.questions[0]}?</h3>
-        <textarea value={props.answer1} onChange={props.handleAnswer1Change}></textarea>
-        <br/>
+        <textarea
+          value={props.answer1}
+          onChange={props.handleAnswer1Change}
+          className="w-4/5 border-2 border-gray-100 shadow mb-4"
+        ></textarea>
+
         <h3>Question {props.questions[1]}?</h3>
-        <textarea value={props.answer2} onChange={props.handleAnswer2Change}></textarea>
-        <br/>
+        <textarea
+          value={props.answer2}
+          onChange={props.handleAnswer2Change}
+          className="w-4/5 border-2 border-gray-100 shadow mb-4"
+        ></textarea>
+
         <h3>Question {props.questions[2]}?</h3>
-        <textarea value={props.answer3} onChange={props.handleAnswer3Change}></textarea>
-        <br/>
+        <textarea
+          value={props.answer3}
+          onChange={props.handleAnswer3Change}
+          className="w-4/5 border-2 border-gray-100 shadow mb-4"
+        ></textarea>
+
         <h3>Question {props.questions[3]}?</h3>
-        <textarea value={props.answer4} onChange={props.handleAnswer4Change}></textarea>
-        <br/>
+        <textarea
+          value={props.answer4}
+          onChange={props.handleAnswer4Change}
+          className="w-4/5 border-2 border-gray-100 shadow mb-4"
+        ></textarea>
+
         <h3>Question {props.questions[4]}?</h3>
-        <textarea value={props.answer5} onChange={props.handleAnswer5Change}></textarea>
-        <br/>
-        <button type="submit" className="submit_button">Submit Answers</button>
+        <textarea
+          value={props.answer5}
+          onChange={props.handleAnswer5Change}
+          className="w-4/5 border-2 border-gray-100 shadow mb-4"
+        ></textarea>
+        <div>
+          <button
+            type="submit"
+            className="bg-blue text-white text-md py-2 px-4 mt-2 inline-flex items-center justify-center text-center border-2 hover:border-blue hover:bg-lblue rounded-xl ease-in-out duration-200 "
+          >
+            Submit Answers
+          </button>
+        </div>
       </form>
     </>
-  )
+  );
 }
 
 export default QuestionsForm;
