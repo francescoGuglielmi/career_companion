8a8fe856f0612fa99c03e0a93dca794b2c6c6b8d
francescoGuglielmi <gug.francesco@gmail.com>
2023-05-03 23:22:37 +0100

    added link on sign in and sign up page and styled feedbacks

--- frontend/src/components/feedback/Feedback.js
+++ frontend/src/components/feedback/Feedback.js
@@ -2,22 +2,24 @@ import React from "react";
 import './Feedback.css'
 
 const Feedback = (props) => {
-  // console.log(props.feedbacks[1].user.firstName)
+
   return (
     <>
-      <form>
-        
+    <div className="feed-container">
+      <form className="search" >
         <span>Search:</span><input className="search_bar" type="text" value={props.searchQuery} onChange={props.handleQueryChange} />
       </form>
-      { props.filteredFeedbacks && props.filteredFeedbacks.map((feedback, index) => ( // border below will have to be removed
-        <div key={index} className="feedback"> 
-        <h2 className="jobPosition">{feedback.jobPosition}<span className="company"> for {feedback.company}</span></h2>
-        <h2 id="author">{feedback.user.firstName} rating: {feedback.rating} </h2>
-        
-        <article className="feedback-content">{feedback.content}</article>
-        
-        </div>
-      ))}
+      
+      <div className="feed">  
+        { props.filteredFeedbacks && props.filteredFeedbacks.map((feedback, index) => ( 
+          <div key={index} className="feedback"> 
+          <h2 id="author">{feedback.user.firstName} Rating: {feedback.rating} </h2>
+            <h2 className="jobPosition">{feedback.jobPosition}<span className="company">for {feedback.company}</span></h2>
+            <article className="feedback-content">{feedback.content}</article>
+          </div>
+        ))}
+      </div>
+    </div>
     </>
   )
 }
