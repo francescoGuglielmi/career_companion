836b5d295ef03ca3f2b930317f8cd5282ee8beb1
Adnan Mann <adnan_mann@outlook.com>
2023-05-04 12:21:19 +0100

    Resume.js completed

--- frontend/src/components/resumeGenerator/Resume.js
+++ frontend/src/components/resumeGenerator/Resume.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import "./resume.css"
+
+function Resume() {
+    return (
+        <div>
+            <NavbarHP className="navbar" />
+            <iframe src="/Resume.html" title="Resume" className="fullscreen" />
+        </div>
+    );
+}
+
+
+export default Resume;
