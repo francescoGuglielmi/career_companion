b2010e5b0effc998b6774009fa778dd9a78cb7fe
Adnan Mann <adnan_mann@outlook.com>
2023-05-04 12:41:56 +0100

    script.js complete without comments

---
+++ frontend/public/assets/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('.repeater').repeater({
+        initEmpty: false,
+        defaultValues: {
+            'text-input': ''
+        },
+        show: function () {
+            $(this).slideDown();
+        },
+        hide: function (deleteElement) {
+            $(this).slideUp(deleteElement);
+            setTimeout(() => {
+                generateCV();
+            }, 500);
+        },
+        isFirstItemUndeletable: true
+    })
+})
