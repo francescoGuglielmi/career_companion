76d16a7aad57196e5c524b077216f913eadb2a0a
francescoGuglielmi <gug.francesco@gmail.com>
2023-06-07 14:20:18 +0100

    added localhost as accepted origin

--- api/app.js
+++ api/app.js
@@ -18,8 +18,10 @@ const cors = require("cors");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+
+const allowedOrigins = ["localhost:3000", "https://career-companion-39n4.onrender.com/"]
 app.use(cors({
-  origin: "https://career-companion-39n4.onrender.com/"
+  origin: allowedOrigins
 }));
 
 // middleware function to check for valid tokens
@@ -49,5 +51,20 @@ app.use("/coverLetterGen", tokenChecker, coverLetterRouter);
 app.use("/applications", tokenChecker, applicationsRouter);
 app.use("/tokens", tokensRouter);
 app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // respond with details of the error
+  res.status(err.status || 500).json({ message: err.message || 'server error' });
+});
 
 module.exports = app;
